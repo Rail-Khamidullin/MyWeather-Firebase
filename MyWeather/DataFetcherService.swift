35ebc274d42d3cefe2e1b6085a7450a21c3e81f2
Rail-Khamidullin <work171@mail.ru>
2022-08-02 15:29:26 +0400

    Изменил порядок записи кода в соответствии с жизненным циклом приложения, добавил модификаторы доступа

--- MyWeather/DataFetcherService.swift
+++ MyWeather/DataFetcherService.swift
@@ -14,26 +14,31 @@ final class DataFetcherService {
     //    Создаём клоужер, который принимает структуру CurrentWeather
     var onCompletion: ((CurrentWeather) -> ())?
     
-//    Создаём внешнюю зависимость с NetworkDataFetcher через Абстракцию (протокол DataFetcher)
-    let dataFetcher: DataFetcher
+    //    Создаём внешнюю зависимость с NetworkDataFetcher через Абстракцию (протокол DataFetcher)
+    private let dataFetcher: DataFetcher
     
     init(dataFetcher: DataFetcher = NetworkDataFetcher()) {
         
         self.dataFetcher = dataFetcher
     }
     
-//    Энум будет определять город в зависимости от координат расположения устройства или введённого города в ручную
+    //    Энум будет определять город в зависимости от координат расположения устройства или введённого города в ручную
     enum RequestType {
         case cityName(city: String)
         case coordinate(latitude: CLLocationDegrees, longitude: CLLocationDegrees)
         
     }
     
-//    Получаем текущую погоду
+    //    Получаем декодированные данные по структуре входящего ответа с сервера и передаём далее
+    private func fetchCurrentWeatherData(urlString: String, completion: @escaping (CurrrentWeatherData?) -> ()) {
+        dataFetcher.genericJSONData(urlString: urlString, response: completion)
+    }
+    
+    //    Получаем текущую погоду
     func fetchCurrentWeather(forRequstType requestType: RequestType) {
         
         var urlString = ""
-//        Поиск по локации или по городу
+        //        Поиск по локации или по городу
         switch requestType {
         case .cityName(city: let city):
             urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(weatherApiKey)&lang=ru&units=metric&lang=ru"
@@ -41,19 +46,14 @@ final class DataFetcherService {
             urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&long=\(longitude)&apikey=\(weatherApiKey)&units=metric&lang=ru"
         }
         
-//        Присваиваем полученные данные weather и обновляем структуру данных CurrentWeather
+        //        Присваиваем полученные декодированные данные weather и обновляем структуру данных CurrentWeather
         let weather = self.fetchCurrentWeatherData(urlString: urlString) { (currentWeatherData) in
-
+            
             if let currentWeatherData = currentWeatherData {
-//                Передаём в форму нашей текущей погоды данные по текущей погоде с сервера
+                //                Передаём в форму нашей текущей погоды данные по текущей погоде с сервера
                 guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData) else { return }
                 self.onCompletion?(currentWeather)
             }
         }
-        }
-    
-//    Получаем декодированные данные по структуре входящего ответа с сервера и передаём далее
-    func fetchCurrentWeatherData(urlString: String, completion: @escaping (CurrrentWeatherData?) -> ()) {
-    dataFetcher.genericJSONData(urlString: urlString, response: completion)
     }
 }
