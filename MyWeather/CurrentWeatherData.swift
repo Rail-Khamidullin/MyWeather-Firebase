07592133b7bb57b496afe38496e3fb1cd963381f
Rail-Khamidullin <work171@mail.ru>
2022-08-02 15:52:42 +0400

    Добавил комментарии для структур и св-в в CurrrentWeatherData, которые приходят от сервера.

--- MyWeather/CurrentWeatherData.swift
+++ MyWeather/CurrentWeatherData.swift
@@ -10,16 +10,26 @@ import Foundation
 //   Структура погоды, которая приходит от сервера
 struct CurrrentWeatherData: Codable {
     
+//    Название города
     let name: String
+//    Структура с температурой, давлением, влажностью
     let main: Main
+//    Структура с id номером для краткого описания текущей погоды
     let weather: [Weather]
+//    Скорость ветра
     let wind: Wind
+//    Структура с рассветом и закатом
     let sys: Sys
 }
+
 struct Main: Codable {
+//    темпертура
     let temp: Double
+//    ощущаемая температура
     let feelsLike: Double
+//    давление
     let pressure: Int
+//    влажность
     let humidity: Int
     
 //    Если мы хотим изменить имя ключа н/п feels_like, то можем использовать enum
@@ -32,14 +42,23 @@ struct Main: Codable {
     }
 }
 
+//   ]структура с краткими погодными условиями
 struct Weather: Codable {
+//    id - цифровое значение, которое имеет краткое описание текущей погоды
     let id: Int
 }
+
+//   Структура со скоростью ветра
 struct Wind: Codable {
+//    скорость ветра
     let speed: Double
 }
+
+//   Структура с временем рассвета и заката
 struct Sys: Codable {
+//    рассвет
     let sunrise: Int
+//    закат
     let sunset: Int
 }
 
