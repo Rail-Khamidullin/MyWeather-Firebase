c961adcb4df3fc5646699bf2e3681b59d2420e03
Rail-Khamidullin <work171@mail.ru>
2022-08-25 13:37:22 +0400

    В контроллер добавил метод, по которому ввод города будет осуществляться только на английском, без возможности переключения на русскоязычную клавиатуру.

--- MyWeather/WeatherViewController.swift
+++ MyWeather/WeatherViewController.swift
@@ -16,16 +16,20 @@ final class WeatherViewController: UIViewController, CLLocationManagerDelegate, 
     
     //    Создаём экземпляр класса DataFetcherService
     let dataFetcherService = DataFetcherService()
+    
     //    Создаём экземпляр класса Animator
     private let animate = Animator()
+    
     //    Создаём менеджера, который будет с приставкой lazy. Если пользователь откажет в предоставлении месторасположения, методы не будут находиться в памяти
     fileprivate lazy var locationManager: CLLocationManager = {
+        
         let locationManager = CLLocationManager()
         locationManager.delegate = self
         //    Необходимо указать точность с которой мы хотим получать информацию. kCLLocationAccuracyKilometer укзывает на точность в километр
         locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
         //        Запрашиваем пользователя к доступу его геопозиции
         locationManager.requestWhenInUseAuthorization()
+        
         return locationManager
     }()
     
@@ -34,19 +38,25 @@ final class WeatherViewController: UIViewController, CLLocationManagerDelegate, 
         
         //        Фон нашего view
         view.backgroundColor = .white
+        
         //        Добавляем WeatherView в view
         view.addSubview(weatherView)
         
-        //        Привязка WeatherView к экранау
+        //        Привязка WeatherView к экрану
         weatherView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
         }
         
+        //        Ввод текста происходит на английском языке. Метод, который не позволяет открыть клавиатуру на русском языке.
+        weatherView.cityTextField.keyboardType = .asciiCapable
+        
         //        Метод для открытия и скрытия клавиатуры
         tapGester()
+        
         //        Получение данных по клавиатуре
         connectToNotificationCenter()
         
+        //        Подписал текстовое поле под делегат, для работы дополнительных функций в клавиатуре
         weatherView.cityTextField.delegate = self
         
         //        Вызов метода, который обновит интерфейс приложения по полученным данным с сервера
@@ -54,8 +64,10 @@ final class WeatherViewController: UIViewController, CLLocationManagerDelegate, 
             guard let self = self else { return }
             self.weatherView.updateInterfaceWith(weather: currentWeather)
         }
+        
         //        Добавим таргет к кнопке поиска погоды
         weatherView.searchWeatherButton.addTarget(self, action: #selector(myButtonPressed(_:)), for: .primaryActionTriggered)
+        
         //        Добавим анимацию для картинки с погодными условиями
         animate.animateAnyObjects(animateObject: weatherView.weatherIconImageView)
     }
